89cbfa78f1dd016465012f7aa6cb376e61486498
cactoes <maartenhvanrooij@gmail.com>
2025-11-25 21:04:46 +0100

    fixed wstring compile issue

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -2,7 +2,7 @@
 #include <htmlparser.hpp>
 
 int main() {
-    parser::HTMLElement document = parser::ParseHTML(L"<h1>Hello</h1>");
-    std::wcout << document.children.at(0).tag << "\n";
+    parser::HTMLElement document = parser::ParseHTML("<h1>Hello</h1>");
+    std::cout << document.children.at(0).tag << "\n";
     return 0;
 }
